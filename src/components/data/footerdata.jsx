29921cf306757efadaf35f19c39b6a1be6530ab3
GilangLiswiyanaPutra <112993380+GilangLiswiyanaPutra@users.noreply.github.com>
2024-09-10 09:23:11 +0700

    rename file

---
+++ src/components/data/footerdata.jsx
@@ -0,0 +1,30 @@
+import { faInstagram, faLinkedin, faFacebook, faWhatsapp, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+export const socialLinks = {
+  linkedin: "https://www.linkedin.com/in/gilang-liswiyana-putra/",
+  instagram: "https://www.instagram.com/palswyn/",
+  github: "https://github.com/GilangLiswiyanaPutra/",
+  facebook: "https://www.facebook.com/gilanglp05/",
+  whatsapp: "https://wa.me/081908647586",
+  mail: "mailto:gilanglputra5@gmail.com"
+};
+
+export const socialPlatforms = [
+  { name: "instagram", icon: faInstagram },
+  { name: "linkedin", icon: faLinkedin },
+  { name: "facebook", icon: faFacebook },
+  { name: "github", icon: faGithub },
+];
+
+export const contactPerson = [
+  { name: "whatsapp", icon: faWhatsapp, text: "081908647586" },
+  { name: "mail", icon: faEnvelope, text: "gilanglputra5@gmail.com"},
+];
+
+export const handleFooter = (platform) => {
+  const url = socialLinks[platform];
+  if (url) {
+    window.open(url, "_blank", "noopener,noreferrer");
+  }
+};
