a5ef6f74439d982eb8780ed2bf202019b64b306f
GilangLiswiyanaPutra <112993380+GilangLiswiyanaPutra@users.noreply.github.com>
2024-09-10 09:20:31 +0700

    create  data components

---
+++ src/components/data/skilldata.jsx
@@ -0,0 +1,19 @@
+import { react, js, html, css, figma, premiere, davinci } from '../../assets/icon';
+
+  export const skills = [
+    {
+      title: "Web Development",
+      description: "Process of creating and maintaining websites, including front-end, back-end, hosting, performance, security, and UX.",
+      icons: [ react, js, html, css ]
+    },
+    {
+      title: "UI & UX Design",
+      description: "UI (User Interface) is the visual design of a website, UX (User Experience) ensures it is easy to use and enjoyable.",
+      icons: [ figma ]
+    },
+    {
+      title: "Video Editing",
+      description: "Video editing is the process of arranging and modifying video footage to create the final product.",
+      icons: [ premiere, davinci ]
+    }
+  ];
